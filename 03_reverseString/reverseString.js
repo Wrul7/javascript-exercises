b972448b4acefa578f6f881f37d1dbdb534f04ab
Russel <170238499+Wrul7@users.noreply.github.com>
2024-11-05 20:27:21 +0800

    Finish javascript exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(normalString) {
+    let returnString = "";
+    for (let i = 0; i < normalString.length; i++) {
+        let letter = normalString.at(-1 - i);
+        returnString = returnString + normalString.at(-1 - i);
+    };
 
+    return returnString;
 };
 
 // Do not edit below this line
