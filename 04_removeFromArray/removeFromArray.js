c13dd85425343249572c63d44df50ce50cae988b
Russel <170238499+Wrul7@users.noreply.github.com>
2024-11-11 20:44:17 +0800

    Finish javascript exercise 4 first attempt

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(original_list, ...toBeRemoved) {
+    let new_list = [];
+
+    for (let i = 0; i < original_list.length; i++) {
+        for (let j = 0; j < toBeRemoved.length; j++) {
+            if (original_list.at(i) !== toBeRemoved.at(j)) {
+                new_list.push(original_list.at(i));
+                break;
+            };
+        };
+    };
+
+    return new_list
 };
 
 // Do not edit below this line
