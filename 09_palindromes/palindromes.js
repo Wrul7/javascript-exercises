44de6337d10db2f017569d723e1c86cf0ce35e63
Russel <170238499+Wrul7@users.noreply.github.com>
2025-03-19 22:04:26 +0800

    Removed console.logs

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,6 @@
 const palindromes = function (string) { 
     let letters = "qwertyuiopasdfghjklzxcvbnm1234567890".split("");
     let arrayFromString = string.toLowerCase().split("").filter(char => letters.some(letter => letter == char));
-    console.log(arrayFromString);
-    console.log(arrayFromString.reverse());
     
     if (arrayFromString.join("") === arrayFromString.reverse().join("")) {
         return true;
@@ -10,8 +8,6 @@ const palindromes = function (string) {
         return false;
     }
 };
-
-console.log(palindromes("racecare!"));
 
 // Do not edit below this line
 module.exports = palindromes;
