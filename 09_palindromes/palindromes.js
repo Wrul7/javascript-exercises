9c6512894e33a3a7c9f9da79358d7830b75aa2f6
Russel <170238499+Wrul7@users.noreply.github.com>
2025-03-19 22:02:48 +0800

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) { 
+    let letters = "qwertyuiopasdfghjklzxcvbnm1234567890".split("");
+    let arrayFromString = string.toLowerCase().split("").filter(char => letters.some(letter => letter == char));
+    console.log(arrayFromString);
+    console.log(arrayFromString.reverse());
+    
+    if (arrayFromString.join("") === arrayFromString.reverse().join("")) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+console.log(palindromes("racecare!"));
 
 // Do not edit below this line
 module.exports = palindromes;
