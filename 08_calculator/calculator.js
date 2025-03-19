59f265729909cb5e974fddcf0151b21b09b9d8b4
Russel <170238499+Wrul7@users.noreply.github.com>
2025-03-19 20:30:29 +0800

    Finish calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return parseInt(a) + parseInt(b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return parseInt(a) - parseInt(b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, addend) => sum + addend, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, factor) => factor * product, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let ans = 1;
+	for (let i = 1; i <= num; i++) {
+    ans = ans * i;
+  };
+  return ans;
 };
 
 // Do not edit below this line
