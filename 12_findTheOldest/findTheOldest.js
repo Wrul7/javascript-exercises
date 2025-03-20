e35ccf23284585ce973cd9f624d9b0a29672d418
Russel <170238499+Wrul7@users.noreply.github.com>
2025-03-20 21:14:17 +0800

    Finish find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const date = new Date();
 
+    let arrOfAge = arr.map(person => {
+        return {
+            name: person["name"],
+            age: person["yearOfDeath"] === undefined ? (date.getFullYear() - person["yearOfBirth"]) : (person["yearOfDeath"] - person["yearOfBirth"]),
+            yearofBirth: person["yearOfBirth"],
+            yearOfDeath: person["yearOfDeath"],
+        }
+        
+    });
+
+    let sortedArr = arrOfAge.sort((a, b) => a["age"] - b["age"]);
+    // console.log(sortedArr);
+
+    return sortedArr[sortedArr.length - 1];
 };
+
+// console.log(findTheOldest(people).name);
 
 // Do not edit below this line
 module.exports = findTheOldest;
