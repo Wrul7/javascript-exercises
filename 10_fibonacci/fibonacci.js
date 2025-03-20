5945aee33370f6fc5265c5b6750dab72a5e5330a
Russel <170238499+Wrul7@users.noreply.github.com>
2025-03-20 20:23:08 +0800

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    let answer = 0;
+    let fArray = [0, 1];
 
+    if (parseInt(number) < 0) {
+        return "OOPS";
+    }
+
+    for (let i = 2; i <= parseInt(number); i++) {
+        fArray.push((fArray[fArray.length - 2]) + (fArray[fArray.length - 1]));
+    }
+
+    return fArray[parseInt(number)];
 };
 
 // Do not edit below this line
